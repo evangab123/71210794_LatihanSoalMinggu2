7b582ca83b572efa75effc595aa18435de4eb99f
EvanderGab <evander.gabriel@ti.ukdw.ac.id>
2022-02-14 12:19:42 +0700

    File Latihan 2 minggu 2

---
+++ 71210794-02_Latihan2.py
@@ -0,0 +1,10 @@
+#menghitung nilai fungsi (x)
+print("+++ Selamat datang di program menghitung fungsi +++ ")
+print("fungsi yang dihitung adalah f(x) = 2x^3 + 2x + 15/x")
+
+#inisiasi varibel dan input
+x = int(input("Masukan Nilai x = "))
+#rumus
+jawaban = (2*(x**3)) + 2*x + (15/x)
+#output
+print("Hasil dari fungsi x dengan nilai x =", x, "adalah", jawaban)
